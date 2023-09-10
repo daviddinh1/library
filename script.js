af57939623d4e3551d7051253ec15b61e5b44313
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2023-09-10 12:34:51 -0500

    added function that reads book object and logs out information

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello world")
+const library = [];
+
+function book(title,author,pages,read){
+ //add constructors here
+ this.title = title;
+ this.author = author;
+ this.pages = pages;
+ this.read = read;
+
+}
+
+function addBookToLibrary(book){
+ //read book information
+ library.push(book);
+
+}
+
+// work on thus tomorrow 
+function display(){
+
+}
+
+const book1 = new book("csm","david","100","read");
+const book2 = new book("lol","dad","120","unread");
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+for(let i =0 ; i<library.length;i++){
+ console.log(library[i]);
+}
