cb9f44039e777e126bf45ecde8bf24f3e410aa1b
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2024-03-05 20:38:45 -0600

    created new book button to add books to webpage; need to create delete book button now

--- script.js
+++ script.js
@@ -40,11 +40,44 @@ let punpun = new Book("punpun","ben","200","unread");
 let vagabond = new Book("vagabond","boo","300","read");
 
 
+
+
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.getElementById("closeBtn");
+
+function newBook(dialog,showButton,closeButton){
+    // "Show the dialog" button opens the dialog modally
+    showButton.addEventListener("click", () => {
+    dialog.showModal();
+    });
+    
+    //issue is data not being submitted learn how to get data
+  
+    // "Close" button closes the dialog
+    closeButton.addEventListener("click", (e) => {
+        let title = document.getElementById("title").value;
+        let author = document.getElementById("author").value;
+        let pages = document.getElementById("pages").value;
+        let read = document.getElementById("read").value;
+        let newBook = new Book(title,author,pages,read);
+
+        addBookToLibrary(newBook);
+        display(myLibrary);
+        e.preventDefault();
+        dialog.close();
+    });
+}
+
+newBook(dialog,showButton,closeButton);
+/*
 addBookToLibrary(hobbit);
 addBookToLibrary(punpun);
 addBookToLibrary(vagabond);
-
+console.log(myLibrary); //its being added but the val not working :c
 
 display(myLibrary);
+*/
 
 
